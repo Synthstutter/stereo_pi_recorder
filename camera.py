665afbfa96bf2374620d468e94f43a308f93da48
Synthstutter <npalermo10@gmail.com>
2016-04-27 10:45:35 -0400

    I don't think I will use this ever'

--- camera.py
+++ camera.py
@@ -1,27 +1,29 @@
-import picamera
+from picamera import PiCamera
 from trigger import Trigger
 from time import sleep
 from led import Led
 
-cam = picamera.PiCamera()
-led = Led()
+# cam = picamera.PiCamera()
+# led = Led()
 
-def record_video(name, seconds):
-    cam.start_recording(name)
-    recording = True
-    while recording:
-        for second in range(seconds/2-1):
-            try:
-                print "recording..."
-                led.on()
-                sleep(1)
-                led.off()
-                sleep(1)
-            except KeyboardInterrupt:  
-                led.cleanup()     
-        recording = False
-    cam.stop_recording()
-    print "end recording"
-    
+class Camera(PiCamera, Led):
+    def __init__(self):
+        self.led_pin = 26
+    # def timed_record_video(self, name, seconds):
+    #     self.PiCamera.start_recording(name)
+    #     while self.PiCamera.recording:
+    #         for second in range(seconds/2-1):
+    #             try:
+    #                 self.recording_led()
+    #             except KeyboardInterrupt:  
+    #                 self.cleanup()     
+    #         self.PiCamera.recording = False
+    #     self.PiCamera.stop_recording()
 
-
+    def recording_led(self):
+        while self.recording: 
+            print "recording..."
+            self.led_on()
+            sleep(1)
+            self.led_off()
+            sleep(1)
